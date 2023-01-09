acbf8007607cb2fd7a6ccfcb5ea1eafb800c05f6
Artem <35739625+Artem26r@users.noreply.github.com>
2023-01-09 18:14:28 +0300

    CL

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import Container from '@mui/material/Container';
 import React from 'react';
 import './App.css';
-import { Dashboard } from './components/Dashboard';
-import {Header} from "./components/Header";
-import {Resume} from "./components/Resume";
 
 
 
 function App() {
   return (
-      <Container maxWidth="xl" className='App'>
-            <Header />
-            <Resume />
-            <Dashboard />
-      </Container>
+      <>
+      </>
   );
 }
 
