0715b0cd50dffa2d5869e104f44c33866bb622e5
Artem <35739625+Artem26r@users.noreply.github.com>
2023-01-09 19:58:52 +0300

    gr cli

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import './App.css';
-
-
+import { HomePage } from "pages/HomePage";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-      <>
-      </>
+    <>
+      <HomePage />
+    </>
   );
 }
 
 export default App;
-
-//auto commit t.me bt.3.0
