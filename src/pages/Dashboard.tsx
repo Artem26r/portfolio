41d79dcf43ea6aa04a2d51cf96f48afebd47d992
Artem <35739625+Artem26r@users.noreply.github.com>
2023-02-08 13:41:44 +0300

    beh api fy

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -12,7 +12,7 @@ export const Dashboard = () => {
     <div className={styles.dashboard}>
       <DashboardWrapperBig classNameGrid={"lg-1"}>
         <BehanceStaticFyApiAdobe
-          count={857}
+          count={1170}
           title="Просмотры"
           linkName="Behance"
           url="https://www.behance.net/artemkrave2003"
@@ -20,7 +20,7 @@ export const Dashboard = () => {
       </DashboardWrapperBig>
       <DashboardWrapperMedium classNameGrid={"md-1"}>
         <BehanceStaticFyApiAdobe
-          count={238}
+          count={408}
           title="Оценки"
           linkName="Behance"
           url="https://www.behance.net/artemkrave2003"
@@ -28,7 +28,7 @@ export const Dashboard = () => {
       </DashboardWrapperMedium>
       <DashboardWrapperMedium classNameGrid={"md-2"}>
         <BehanceStaticFyApiAdobe
-          count={27}
+          count={32}
           title="Подписчики"
           linkName="Behance"
           url="https://www.behance.net/artemkrave2003"
